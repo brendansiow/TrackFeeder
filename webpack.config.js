abdd939210581ec3e7a0a35d4357bbfff8b22773
Brendan <brendansiowx96@gmail.com>
2016-10-19 11:03:36 +0800

    Revert "RE-Modified the starter files"
    This reverts commit 75a9b22af40ee174d051bd166bb37d1ce8580609.

--- webpack.config.js
+++ webpack.config.js
@@ -2,36 +2,30 @@ var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
-    entry: [
-        './src/index'
-    ],
-    module: {
-        loaders: [{
-                test: /\.js?$/,
-                loader: 'babel',
-                exclude: /node_modules/,
-                query: {
-                    presets: ['react','es2015']
-                }
-            },
-            { test: /\.s?css$/, loader: 'style!css!sass' },
-        ]
-    },
-    resolve: {
-        extensions: ['', '.js']
-    },
-    output: {
-        path: path.join(__dirname, '/dist'),
-        publicPath: '/',
-        filename: 'bundle.js'
-    },
-    devServer: {
-        contentBase: './dist',
-        hot: true
-    },
-    plugins: [
-        new webpack.optimize.OccurenceOrderPlugin(),
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoErrorsPlugin()
+  entry: [
+    './src/index'
+  ],
+  module: {
+    loaders: [
+      { test: /\.js?$/, loader: 'babel', exclude: /node_modules/ },
+      { test: /\.s?css$/, loader: 'style!css!sass' },
     ]
+  },
+  resolve: {
+    extensions: ['', '.js']
+  },
+  output: {
+    path: path.join(__dirname, '/dist'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ]
 };
