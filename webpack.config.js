991b45add2563b6859b09a1e8c33ca8ee7db934e
Brendan <brendansiowx96@gmail.com>
2017-07-07 15:36:18 +0800

    warning resolved

--- webpack.config.js
+++ webpack.config.js
@@ -33,10 +33,5 @@ module.exports = {
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin(),
-   new webpack.DefinePlugin({
-    "process.env": {
-      "NODE_ENV": JSON.stringify("development")
-    }
-  })
   ]
 };
